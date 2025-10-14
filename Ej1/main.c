96f4834bcc8cc12550ba6f3c15764eebbe0cb6c9
FacuA0 <facundoamarilla28@gmail.com>
2025-10-13 23:17:41 -0300

    Ej1: Probando memoria compartida

--- Ej1/main.c
+++ Ej1/main.c
@@ -2,13 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+typedef struct {
+    int id;
+    int num1, num2;
+    char nombre[31];
+    int num4;
+} Registro;
+
+int manejarArgumentos(int argc, char* argv[], int* procesos, int* registros);
 
 int main(int argc, char* argv[])
 {
-    int pid, i, cantProcesos = -1, cantRegistros = -1;
+    int pid, i, idMem, cantProcesos = -1, cantRegistros = -1;
+    void *mem;
+
+    if (manejarArgumentos(argc, argv, &cantProcesos, &cantRegistros)) {
+        return 1;
+    }
+
+    idMem = shm_open("/mem_tpso", O_CREAT | O_RDWR, 0600);
+    ftruncate(idMem, sizeof(Registro) * cantProcesos);
+    mem = mmap(NULL, sizeof(Registro) * cantProcesos, PROT_READ | PROT_WRITE, MAP_SHARED, idMem, 0);
+
+    printf("Ejercicio 1!\n");
+
+    pid = fork();
+    if (pid == 0) {
+
+        sprintf(mem, "Hola a todos\n");
+        printf("PID 0, soy el hijo.\n");
+        sleep(10);
+
+    }
+    else {
+        printf("PID %d, soy el padre.\n", pid);
+        sleep(11);
+        printf("%s", mem);
+    }
+
+    munmap(mem, sizeof(Registro) * cantProcesos);
+    shm_unlink("/mem_tpso");
+
+    return 0;
+}
+
+int manejarArgumentos(int argc, char* argv[], int* procesos, int* registros) {
+    int i;
 
     if (argc < 3) {
-        printf("Pocos parametros.\n");
+        printf("Pocos parámetros.\n");
         printf("Uso: Ej1 --procesos=<procesos> --registros=<registros>\n");
         printf("  o: Ej1 -p=<procesos> -r=<registros>\n");
         return 1;
@@ -16,37 +63,27 @@ int main(int argc, char* argv[])
 
     for (i = 1; i < argc; i++) {
         if (strstr(argv[i], "-p=") == argv[i]) {
-            cantProcesos = atoi(argv[i] + 3);
+            *procesos = atoi(argv[i] + 3);
         }
         else if (strstr(argv[i], "-r=") == argv[i]) {
-            cantRegistros = atoi(argv[i] + 3);
+            *registros = atoi(argv[i] + 3);
         }
         else if (strstr(argv[i], "--procesos=") == argv[i]) {
-            cantProcesos = atoi(argv[i] + 11);
+            *procesos = atoi(argv[i] + 11);
         }
         else if (strstr(argv[i], "--registros=") == argv[i]) {
-            cantRegistros = atoi(argv[i] + 12);
+            *registros = atoi(argv[i] + 12);
         }
     }
 
-    if (cantProcesos <= 0) {
-        printf("Falto especificar cantidad de procesos o se especifico un numero menor a 0.\n");
+    if (*procesos <= 0) {
+        printf("Faltó especificar cantidad de procesos o se especifico un numero menor a 0.\n");
         return 1;
     }
 
-    if (cantRegistros <= 0) {
-        printf("Falto especificar cantidad de registros o se especifico un numero menor a 0.\n");
+    if (*registros <= 0) {
+        printf("Faltó especificar cantidad de registros o se especifico un numero menor a 0.\n");
         return 1;
-    }
-
-    printf("Ejercicio 1!\n");
-
-    pid = fork();
-    if (pid == 0) {
-        printf("PID 0, soy el hijo.\n");
-    }
-    else {
-        printf("PID %d, soy el padre.\n", pid);
     }
 
     return 0;
